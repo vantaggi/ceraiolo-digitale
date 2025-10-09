a0eb9515fadca2441953562952b21548e76aaac8
Mattia Vantaggi <mattia.vantaggi@artgroup-spa.com>
2025-10-09 11:05:55 +0200

    refactor: consolidate MockURL class definition for better code organization in test setup

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -1,22 +1,53 @@
 import { vi } from 'vitest'
 
-class MockURL {
-  constructor(url) {
-    this.href = url
-  }
-  toString() {
-    return this.href
-  }
-  get protocol() {
-    return 'https:'
-  }
-  get hostname() {
-    return 'localhost'
-  }
-}
-
 vi.mock('whatwg-url', async () => {
   return {
-    URL: MockURL,
+    URL: class MockURL {
+      constructor(url) {
+        this.href = url
+      }
+      toString() {
+        return this.href
+      }
+      get protocol() {
+        return 'https:'
+      }
+      get hostname() {
+        return 'localhost'
+      }
+    },
+  }
+})
+
+vi.mock('jsdom', async () => {
+  const jsdom = await vi.importActual('jsdom')
+
+  class MockURL {
+    constructor(url) {
+      this.href = url
+    }
+    toString() {
+      return this.href
+    }
+    get protocol() {
+      return 'https:'
+    }
+    get hostname() {
+      return 'localhost'
+    }
+  }
+
+  return {
+    default: {
+      JSDOM: class {
+        constructor(html, options) {
+          const dom = new jsdom.JSDOM(html, options)
+          this.window = dom.window
+          this.window.URL = MockURL
+          this.window.document = dom.window.document
+          return this
+        }
+      },
+    },
   }
 })
